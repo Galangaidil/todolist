2c03d54c5448c8603f63ad9af0f9ef253e815657
Galangaidil <galangaidil45@gmail.com>
2021-03-22 00:40:46 +0700

    Menambahkan required pada form sign up dan sign in

--- index.php
+++ index.php
@@ -84,11 +84,11 @@ if (isset($_POST['register'])) {
           <h2 class="title">Sign in</h2>
           <div class="input-field">
             <i class="fas fa-user"></i>
-            <input type="text" name="username" placeholder="Username">
+            <input type="text" name="username" placeholder="Username" required>
           </div>
           <div class="input-field">
             <i class="fas fa-lock"></i>
-            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password" placeholder="Password" required>
           </div>
           <input type="submit" name="login" value="Login" class="btn solid">
 
@@ -113,15 +113,15 @@ if (isset($_POST['register'])) {
           <h2 class="title">Sign up</h2>
           <div class="input-field">
             <i class="fas fa-user"></i>
-            <input type="text" name="username" placeholder="Username">
+            <input type="text" name="username" placeholder="Username" required>
           </div>
           <div class="input-field">
             <i class="fas fa-envelope"></i>
-            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="email" placeholder="Email" required>
           </div>
           <div class="input-field">
             <i class="fas fa-lock"></i>
-            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password" placeholder="Password" required>
           </div>
           <input type="submit" name="register" value="Sign up" class="btn solid">
 
